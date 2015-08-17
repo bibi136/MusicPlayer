8801b45edad54a61a4df76246af8582c9482727b
HungNT <ilu199x@gmail.com>
2015-08-17 13:09:34 +0700

    Adding layout for music player

--- app/src/main/java/com/hungnt/customlogin/ListSongActivity.java
+++ app/src/main/java/com/hungnt/customlogin/ListSongActivity.java
@@ -5,27 +5,20 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.hungnt.customlogin.Objs.SongInfo;
 import com.hungnt.customlogin.fragments.ListSongFragment;
-import com.hungnt.customlogin.fragments.MusicPlayerFragment;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import java.util.ArrayList;
 
 
-public class ListSongActivity extends FragmentActivity implements AdapterView.OnItemClickListener {
+public class ListSongActivity extends FragmentActivity {
 
     private static final String TAG = "hung";
     public ImageLoader imageLoader = ImageLoader.getInstance();
@@ -40,6 +33,7 @@ public class ListSongActivity extends FragmentActivity implements AdapterView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_song);
         songs = new ArrayList<>();
+
         //Create song
         createSong();
         imageLoader.init(ImageLoaderConfiguration.createDefault(getBaseContext()));
@@ -49,6 +43,7 @@ public class ListSongActivity extends FragmentActivity implements AdapterView.On
         tabs.setIndicatorColor(0xFF3F9FE0);
         tabs.setBackgroundColor(0xCC5c5b78);
         tabs.setTextColor(0xffffffff);
+
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         ListPagerAdapter pagerAdapter = new ListPagerAdapter(getSupportFragmentManager());
 
@@ -58,115 +53,6 @@ public class ListSongActivity extends FragmentActivity implements AdapterView.On
         tv_name_song_playing = (TextView) findViewById(R.id.tv_name_playing_song);
         tv_artist_playing_song = (TextView) findViewById(R.id.tv_artist_playing_song);
         layoutPlaying = findViewById(R.id.layout_playing);
-
-
-    }
-
-    private void createSong() {
-        songs.add(new SongInfo("See You Again", "Wizkalifa", 444, 2321, 12313, R.drawable.artwork_native));
-        songs.add(new SongInfo("Chandelier", "Sia", 44, 55, 33, R.drawable.artwork_yearendhot));
-        songs.add(new SongInfo("Will It Rain", "Bruno Mars,", 1234, 4566, 23422, R.drawable.artwork_hooligans));
-        songs.add(new SongInfo("Good For You", "Selena", 123, 434, 1123, R.drawable.artwork_maron5));
-        songs.add(new SongInfo("Imagine", "Elton John", 1123, 4342, 1215, R.drawable.artwork_fifty));
-        songs.add(new SongInfo("Rocket Man", "John Lennon"));
-        songs.add(new SongInfo("Moonlight Mile", "Rolling Stones", 1122, 3332, 2233, R.drawable.artwork_hotel));
-        songs.add(new SongInfo("American Pie", "Don McLean"));
-        songs.add(new SongInfo("Your Song", "Elton John", 1, 43, 555, R.drawable.artwork_rockstar));
-        songs.add(new SongInfo("Everyday People", "Sly & The Family Stone"));
-        songs.add(new SongInfo("Space Oddity", "David Bowie", 2311, 4443, 235, R.drawable.artwork_yeu));
-//        for (int i = 0; i < 50; i++) {
-//            songs.add(new SongInfo("See You Again", "Wizkalifa", 444, 2321, 12313, R.drawable.img_artwork));
-//        }
-
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        //TODO bat su kien play nhac khi chon 1 bai hat
-        Log.d(TAG, "onItemClick ");
-
-        layoutPlaying.setVisibility(View.VISIBLE);
-        tv_name_song_playing.setText(songs.get(position).getName());
-        tv_artist_playing_song.setText(songs.get(position).getAuthor());
-
-        Bundle bundle = new Bundle();
-        bundle.putInt("pos", position);
-
-        MusicPlayerFragment playerFragment = new MusicPlayerFragment();
-        playerFragment.setArguments(bundle);
-
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        fragmentTransaction.replace(R.id.mainLayout, playerFragment).addToBackStack(null).commit();
-
-
-    }
-
-
-    //Adapter
-    class ListSong extends BaseAdapter {
-
-
-        @Override
-        public int getCount() {
-            return songs.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            View view = convertView;
-            ViewHolder viewHolder;
-
-            if (view == null) {
-
-                //Inflate layout
-                view = getLayoutInflater().inflate(R.layout.item_listview_song, parent, false);
-
-                //set new viewHolder
-                viewHolder = new ViewHolder();
-                viewHolder.tvName = (TextView) view.findViewById(R.id.tvName);
-                viewHolder.tvAuthor = (TextView) view.findViewById(R.id.tvAuthor);
-//                viewHolder.tvLiked = (TextView) view.findViewById(R.id.tvLiked);
-//                viewHolder.tvListened = (TextView) view.findViewById(R.id.tvListened);
-//                viewHolder.tvDownloaded = (TextView) view.findViewById(R.id.tvDownloaded);
-//                viewHolder.ivArtwork = (ImageView) view.findViewById(R.id.ivArtwork);
-
-                //store the holder
-                view.setTag(viewHolder);
-            } else {
-                viewHolder = (ViewHolder) view.getTag();
-            }
-
-            SongInfo songInfo = songs.get(position);
-
-            if (songInfo != null) {
-                viewHolder.tvName.setText(songInfo.getName());
-                viewHolder.tvAuthor.setText(songInfo.getAuthor());
-//                viewHolder.tvLiked.setText(songInfo.getLiked() + "");
-//                viewHolder.tvDownloaded.setText(songInfo.getDownloaded() + "");
-//                viewHolder.tvListened.setText(songInfo.getListened() + "");
-//                viewHolder.ivArtwork.setImageResource(songInfo.getCover());
-            }
-            return view;
-        }
-
-    }
-
-    static class ViewHolder {
-        TextView tvName, tvAuthor, tvLiked, tvDownloaded, tvListened;
-        ImageView ivArtwork;
     }
 
     private class ListPagerAdapter extends FragmentPagerAdapter {
@@ -194,4 +80,19 @@ public class ListSongActivity extends FragmentActivity implements AdapterView.On
             return TITLES.length;
         }
     }
+
+    private void createSong() {
+        songs.add(new SongInfo("See You Again", "Wizkalifa", 444, 2321, 12313, R.drawable.artwork_native));
+        songs.add(new SongInfo("Chandelier", "Sia", 44, 55, 33, R.drawable.artwork_yearendhot));
+        songs.add(new SongInfo("Will It Rain", "Bruno Mars,", 1234, 4566, 23422, R.drawable.artwork_hooligans));
+        songs.add(new SongInfo("Good For You", "Selena", 123, 434, 1123, R.drawable.artwork_maron5));
+        songs.add(new SongInfo("Imagine", "Elton John", 1123, 4342, 1215, R.drawable.artwork_fifty));
+        songs.add(new SongInfo("Rocket Man", "John Lennon"));
+        songs.add(new SongInfo("Moonlight Mile", "Rolling Stones", 1122, 3332, 2233, R.drawable.artwork_hotel));
+        songs.add(new SongInfo("American Pie", "Don McLean"));
+        songs.add(new SongInfo("Your Song", "Elton John", 1, 43, 555, R.drawable.artwork_rockstar));
+        songs.add(new SongInfo("Everyday People", "Sly & The Family Stone"));
+        songs.add(new SongInfo("Space Oddity", "David Bowie", 2311, 4443, 235, R.drawable.artwork_yeu));
+    }
+
 }
